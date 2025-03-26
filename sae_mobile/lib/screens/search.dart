9995be22a5f242e288728b298e47f1a84793c78d
Joann45 <joann.raignault@gmail.com>
2025-03-26 10:41:45 +0100

    ajout searchbar + dossier template (restorants cards)

--- sae_mobile/lib/screens/search.dart
+++ sae_mobile/lib/screens/search.dart
@@ -13,10 +13,43 @@ class SearchScreen extends StatelessWidget {
         style: TextStyle(color: Color(0xFFBBBBBB))),
       ),
       body: Center(
-        child: Text("Search",
-          style: TextStyle(color: Color(0xFFBBBBBB))
-        ),
-      ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text("Search",
+              style: TextStyle(
+                color: Color(0xFFBBBBBB))),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 8,
+                vertical: 16),
+              child: TextField(
+                
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xFFBBBBBB),
+                  hintText: 'Rechercher un resto',
+
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Color(0xFFBBBBBB), width: 2.0),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Color(0xFFBBBBBB), width: 2.0),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Color(0xFFBBBBBB), width: 2.0),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+            )
+          ],
+        )
+      )
     );
   }
 }
